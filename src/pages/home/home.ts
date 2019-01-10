984efbea4adeb4dd0c51fd27882fa1146ddd0e8f
syafiqah <syaf@ubuntu18.04-lts>
2019-01-10 14:51:59 +0800

    Add flashlight plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Flashlight } from '@ionic-native/flashlight';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  // constructor(public navCtrl: NavController) {
 
+  // }
+  constructor(private flashlight: Flashlight){
+
+  }
+
+  turnOn(){
+    this.flashlight.switchOn();
+  }
+  turnOff(){
+    this.flashlight.switchOff();
   }
 
 }
